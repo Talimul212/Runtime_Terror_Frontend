f8890625c1c4a5a419537b31f8ef64e95a5957a2
Talimul212 <islamtalimul0@gmail.com>
2025-11-12 22:09:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Runtime Terror");
+});
+
+app.listen(port, () => {
+  console.log(`Runtime Terror app listening on port ${port}`);
+});
